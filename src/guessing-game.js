5194b52ca38797373ed2070a87bd2dd00c5c2058
AnStarost <anstarost.mail@gmail.com>
2022-08-16 12:43:38 +0300

    feat: new task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor(number,result) {
+        //this.guess_user;
+        this.min;
+        this.max;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+       return Math.round((this.max + this.min) / 2);
     }
 
     lower() {
-
+        return this.max = Math.round((this.max + this.min) / 2);
     }
 
     greater() {
-
+        return this.min = Math.round((this.max + this.min) / 2);
     }
 }
 
